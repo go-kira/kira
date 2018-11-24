be9c8ff40db71bcd86989206ce67c216fdabac4b
Rachid Lafriakh <lafriakh.rachid@gmail.com>
2018-11-24 20:23:42 +0100

    update parsejson method

--- context_json.go
+++ context_json.go
@@ -23,7 +23,7 @@ func (c *Context) WantsJSON() bool {
 
 // ParseJSON - convert json from request body to interface.
 func (c *Context) ParseJSON(dst interface{}) {
-	err := json.NewDecoder(c.Request().Body).Decode(&dst)
+	err := json.NewDecoder(c.Request().Body).Decode(dst)
 	if err != nil {
 		c.Error(err)
 	}
