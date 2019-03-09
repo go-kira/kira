148737250d6291fbc3ac233966500f6f0b67c014
Lafriakh Rachid <lafriakh.rachid@gmail.com>
2019-03-09 19:03:25 +0000

    Added tests for not found and custom handlers

---
+++ tests/kira_not_found_test.go
@@ -0,0 +1,53 @@
+package tests
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/go-kira/kira"
+)
+
+func TestNotFound(t *testing.T) {
+	s := endpoint("GET", "/method", func(c *kira.Context) {
+		c.String("GET")
+	})
+
+	// Request
+	res, _ := http.Get(url(s, "/not_exists"))
+	defer res.Body.Close()
+
+	// Assert
+	if res.StatusCode != 404 {
+		t.Errorf("expect stauts: `404`, have: %d", res.StatusCode)
+	}
+}
+
+func TestCustomNotFound(t *testing.T) {
+	app := kira.New()
+
+	app.NotFound(func(c *kira.Context) {
+		c.HeaderStatus(http.StatusNotFound)
+		c.String("custom 404")
+	})
+
+	// Server
+	s := httptest.NewServer(app.RegisterRoutes())
+
+	// Request
+	res, _ := http.Get(url(s, "/not_exists"))
+	defer res.Body.Close()
+
+	// Assert
+	if res.StatusCode != 404 {
+		t.Errorf("expect stauts: `404`, have: %d", res.StatusCode)
+	}
+	// Assert
+	// Get the response as string.
+	content := contentS(res.Body)
+
+	// Assert
+	if content != "custom 404" {
+		t.Errorf("expect: `custom 404`, have: %s", content)
+	}
+}
