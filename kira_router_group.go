53f53f608adfc5d12484e62024755c1ec50f226c
Lafriakh Rachid <lafriakh.rachid@gmail.com>
2019-03-09 23:48:43 +0000

    Added support to grouped routes| now you can group your routes

---
+++ kira_router_group.go
@@ -0,0 +1,63 @@
+package kira
+
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+// Group represent routes group.
+type Group struct {
+	app    *App
+	prefix string
+}
+
+// Group adds a prefix to the given routes.
+func (app *App) Group(prefix string, group func(Group)) {
+	g := Group{
+		app:    app,
+		prefix: prefix,
+	}
+
+	// Register the group routes.
+	group(g)
+}
+
+func (g Group) path(path string) string {
+	return httprouter.CleanPath(g.prefix + httprouter.CleanPath(path))
+}
+
+// Get is a shortcut for app.Get with the group prefix.
+func (g Group) Get(path string, handler HandlerFunc, middlewares ...Middleware) {
+	g.app.Get(g.path(path), handler, middlewares...)
+}
+
+// Get is a shortcut for app.Head with the group prefix.
+func (g Group) Head(path string, handler HandlerFunc, middlewares ...Middleware) {
+	g.app.Head(g.path(path), handler, middlewares...)
+}
+
+// Get is a shortcut for app.Post with the group prefix.
+func (g Group) Post(path string, handler HandlerFunc, middlewares ...Middleware) {
+	g.app.Post(g.path(path), handler, middlewares...)
+}
+
+// Get is a shortcut for app.Put with the group prefix.
+func (g Group) Put(path string, handler HandlerFunc, middlewares ...Middleware) {
+	g.app.Put(g.path(path), handler, middlewares...)
+}
+
+// Get is a shortcut for app.Patch with the group prefix.
+func (g Group) Patch(path string, handler HandlerFunc, middlewares ...Middleware) {
+	g.app.Patch(g.path(path), handler, middlewares...)
+}
+
+// Get is a shortcut for app.Delete with the group prefix.
+func (g Group) Delete(path string, handler HandlerFunc, middlewares ...Middleware) {
+	g.app.Delete(g.path(path), handler, middlewares...)
+}
+
+// Get is a shortcut for app.ServeFiles with the group prefix.
+func (g Group) ServeFiles(path string, root http.FileSystem) {
+	g.app.ServeFiles(g.path(path), root)
+}
