cc86d99d2fd7e46cc92c048161e8a70d10dd6f67
Lafriakh Rachid <lafriakh.rachid@gmail.com>
2019-03-19 15:22:05 +0100

    Improve Recover middleware

--- go.mod
+++ go.mod
@@ -3,9 +3,12 @@ module github.com/go-kira/kira
 go 1.12
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/go-kira/config v0.1.1
 	github.com/go-kira/log v0.1.3
+	github.com/google/uuid v1.1.1
 	github.com/gorilla/csrf v1.5.1
 	github.com/julienschmidt/httprouter v1.2.0
+	golang.org/x/crypto v0.0.0-20190313024323-a1f597ede03a
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0
 )
--- middlewares/recover/recover.go
+++ middlewares/recover/recover.go
@@ -1,11 +1,25 @@
 package recover
 
 import (
+	"fmt"
 	"net/http"
 	"runtime"
 
 	"github.com/go-kira/kira"
 )
+
+// ErrorFrame ...
+type ErrorFrame struct {
+	File string `json:"file"`
+	Func string `json:"func"`
+	Line int    `json:"line"`
+}
+
+// ErrorJSON ...
+type ErrorJSON struct {
+	Message string       `json:"message"`
+	Frames  []ErrorFrame `json:"frames,omitempty"`
+}
 
 // Recover - Middleware
 type Recover struct{}
@@ -33,23 +47,50 @@ func (rc *Recover) Middleware(ctx *kira.Context, next kira.HandlerFunc) {
 
 			// if the debug mode is enabled, add the stack to the error view
 			if ctx.Config().GetBool("app.debug", false) {
-				if ctx.ViewExists("error/debug") {
-					ctx.View("errors/debug", kira.ViewData{
-						"message": r,
-						"frames":  getFrames(100),
-					})
-				} else {
-					ctx.String("We're sorry, but something went wrong. \n\n")
-					ctx.Stringf("Message: %s\nFrames:\n\n", r)
+				if ctx.WantsJSON() { // JSON
+					var frames []ErrorFrame
 					for _, frame := range getFrames(100) {
-						ctx.Stringf("Func: %s \nFile: %s \nLine: %d\n\n", frame.Func.Name(), frame.File, frame.Line)
+						frames = append(frames, ErrorFrame{
+							File: frame.File,
+							Func: frame.Func.Name(),
+							Line: frame.Line,
+						})
+					}
+					ctx.JSON(ErrorJSON{
+						Message: fmt.Sprintf("%s", r),
+						Frames:  frames,
+					})
+				} else { // HTML
+					if ctx.ViewExists("error/debug") {
+						ctx.View("errors/debug", kira.ViewData{
+							"message": r,
+							"frames":  getFrames(100),
+						})
+					} else {
+						ctx.String("We're sorry, but something went wrong. \n\n")
+						ctx.Stringf("Message: %s\nFrames:\n\n", r)
+						for _, frame := range getFrames(100) {
+							ctx.Stringf("Func: %s \nFile: %s \nLine: %d\n\n", frame.Func.Name(), frame.File, frame.Line)
+						}
 					}
 				}
+
 				return
 			}
 
-			// display error page
-			ctx.View("errors/500")
+			// Normal mode
+			if ctx.WantsJSON() {
+				ctx.JSON(ErrorJSON{
+					Message: fmt.Sprintf("%s", r),
+				})
+			} else {
+				if ctx.ViewExists("errors/500") {
+					ctx.View("errors/500")
+				} else {
+					ctx.String(`<html><head><title>Internal Server Error</title></head><body>We're sorry, but something went wrong.</body></html>`)
+				}
+			}
+
 			return
 		}
 	}()
