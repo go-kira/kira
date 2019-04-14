cbd5f9c28c3ae2d70015c749e0a5e4f93c84a2d5
Lafriakh Rachid <lafriakh@lafriakh>
2019-04-14 06:06:08 -0500

    change config file to env

--- kira_config.go
+++ kira_config.go
@@ -30,8 +30,8 @@ func getEnv() string {
 }
 
 func getConfig() *config.Config {
-	var files = []string{"./testdata/config", "./config", "./config/config"}
-	var env = fmt.Sprintf("./config.%s", getEnv())
+	var files = []string{"./testdata/env", "./env", "./config/env"}
+	var env = fmt.Sprintf("./env.%s", getEnv())
 
 	if _, err := os.Stat(env); !os.IsNotExist(err) {
 		files = append(files, env)
