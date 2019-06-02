8449033593b8cd7a1f64875b67fca0f711fe4895
Rachid Lafriakh <lafriakh.rachid@gmail.com>
2019-06-02 19:18:38 +0000

    update in gzip

--- middlewares/gzip/gzip.go
+++ middlewares/gzip/gzip.go
@@ -60,5 +60,8 @@ func (w *gzipResponseWriter) WriteHeader(code int) {
 	w.ResponseWriter.WriteHeader(code)
 }
 func (w *gzipResponseWriter) Write(b []byte) (int, error) {
+	if w.Header().Get("Content-Type") == "" {
+		w.Header().Set("Content-Type", http.DetectContentType(b))
+	}
 	return w.Writer.Write(b)
 }
